a229ed6ef2d9021a3a1e8888d3d0444c528f59da
fatihbaycu <fatih.baycu@gmail.com>
2026-02-21 19:50:07 +0300

    TestsController added.

---
+++ CI_CD_Test.WebAPI/Controllers/TestsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CI_CD_Test.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Message = "CI/CD is working 🚀",
+                ServerTime = DateTime.Now,
+                Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+            });
+        }
+    }
+}
